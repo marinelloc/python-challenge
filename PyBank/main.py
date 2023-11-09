77fc7070d87d634c41c1b0a98728809d58c9b74a
ChristopherMarinello <marinello.christopher@gmail.com>
2023-11-09 14:46:18 -0500

    trouble shoot average before class

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,27 +4,37 @@ import csv
 # budget_csv = os.path.join("..", "Resources", "budget_data.csv")
 budget_csv = os.path.join(r"C:\Users\marin\repo\python-challenge\PyBank\Resources\budget_data.csv")
 
+# declaring variables
 months = 0
 bottomline = 0
 profit_loss = []
 month_list = []
 change = 0
 
+# logic for opening up csv and declaring header
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
-
+    # initite iterator for csv
     for row in csvreader:
+        # tallying all months
         months += 1
+        # summing all profits/losses
         bottomline += int(row[1])
+        # addinng all profit changes to list
         profit_loss.append((int(row[1])-change))
+        # storing current rows profit to be used in comparsion against next row
         change = int(row[1])
+        # adding all dates to list
         month_list.append(row[0])
-    
+
+    #calulates average of profits/loss
     average_change   = round(sum(profit_loss)/months)
+    # finds postion in list of greastest and worst profits/losses
     greatest_index   = profit_loss.index(max(profit_loss))
     worst_index      = profit_loss.index(min(profit_loss))
-
+    
+    # print results to terminal
     print("""Financial Analysis
 ----------------------------""")
     print(f'Total Months: {months}')
@@ -39,7 +49,7 @@ results_csv = os.path.join(r"C:\Users\marin\repo\python-challenge\PyBank\Analysi
 #  Open the output file
 with open(results_csv, "w") as datafile:
     writer = csv.writer(datafile)
-
+     # print results to output csv
     writer.writerow(['Financial Analysis'])
     writer.writerow(['----------------------------'])
     writer.writerow([f'Total Months: {months}'])
